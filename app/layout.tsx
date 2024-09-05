42c065b925250788fb92e4b41d3b3f1a0f0c77c0
Pratim <hpro373@gmail.com>
2024-09-06 01:35:33 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} min-h-screen      overflow-hidden ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
