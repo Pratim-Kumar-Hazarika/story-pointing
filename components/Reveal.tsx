d774707be77ced41179695a9179c3e30c5e394ea
Pratim <hpro373@gmail.com>
2024-09-07 15:57:52 +0530

    Update Reveal.tsx

--- components/Reveal.tsx
+++ components/Reveal.tsx
@@ -3,15 +3,16 @@ import React from "react";
 import { HoverBorderGradient } from "./HoverBorderGradient";
 
 
-export function Reveal() {
+export function Reveal({title}:{title:string}) {
   return (
-    <div className=" flex justify-center text-center z-50 mt-7  ">
+    <div className=" flex justify-center text-center z-50   ">
       <HoverBorderGradient
+      leftSideBar={false}
         containerClassName="rounded-md    "
         as="button"
         className="bg-black  text-white flex items-center space-x-2"
       >
-        <span className=" font-medium  text-lg">Reveal Estimates</span>
+        <span className=" font-medium  text-lg">{title}</span>
       </HoverBorderGradient>
     </div>
   );
