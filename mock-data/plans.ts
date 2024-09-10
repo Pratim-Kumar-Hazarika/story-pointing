001fd7b61d556a82fab7f5390dc04a983be6a86c
Pratim <hpro373@gmail.com>
2024-09-10 23:47:04 +0530

    Create plans.ts

---
+++ mock-data/plans.ts
@@ -0,0 +1,31 @@
+export const plans = [
+  {
+    type: "Free Plan",
+    price: "$0",
+    frequency: "/month",
+    description:
+      "Perfect for trying out our product and the functionalities we provide.",
+    features: [
+      { title: "3 Sessions, No Sessions stored.", description: "1 hour ago" },
+      { title: "30 Team Members.", description: "1 hour ago" },
+      { title: "48 hours support window.", description: "2 hours ago" },
+    ],
+    buttonText: "Start for Free",
+    bgColor: "bg-black",
+    borderColor: "border-neutral-800",
+  },
+  {
+    type: "Pro Plan",
+    price: "$30",
+    frequency: "/month",
+    description: "For advanced features & priority support.",
+    features: [
+      { title: "Unlimited, Past Sessions.", description: "1 hour ago" },
+      { title: "100 Team Members.", description: "1 hour ago" },
+      { title: "24 hour support window.", description: "2 hours ago" },
+    ],
+    buttonText: "Upgrade to Pro",
+    bgColor: "bg-black",
+    borderColor: "border-neutral-800",
+  },
+];
