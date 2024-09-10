e36b2e6be86cac1144dd0f5cedaed7f571f12fd6
Pratim <hpro373@gmail.com>
2024-09-10 23:47:16 +0530

    Update PricingCard.tsx

--- components/PricingCard.tsx
+++ components/PricingCard.tsx
@@ -1,61 +1,34 @@
-import { cn } from "@/lib/utils"
-
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Reveal } from "./Reveal"
-
-const plans = [
-  {
-    type: "Free Plan",
-    price: "$0",
-    frequency: "/month",
-    description: "Perfect for trying out our product and the functionalities we provide.",
-    features: [
-      { title: "3 Sessions, No Sessions stored.", description: "1 hour ago" },
-      { title: "30 Team Members.", description: "1 hour ago" },
-      { title: "48 hours support window.", description: "2 hours ago" }
-    ],
-    buttonText: "Start for Free",
-    bgColor: "bg-black",
-    borderColor: "border-neutral-800"
-  },
-  {
-    type: "Pro Plan",
-    price: "$30",
-    frequency: "/month",
-    description: "For advanced features & priority support.",
-    features: [
-      { title: "Unlimited, Past Sessions.", description: "1 hour ago" },
-      { title: "100 Team Members.", description: "1 hour ago" },
-      { title: "24 hour support window.", description: "2 hours ago" }
-    ],
-    buttonText: "Upgrade to Pro",
-    bgColor: "bg-black",
-    borderColor: "border-neutral-800"
-  }
-]
-
-type CardProps = React.ComponentProps<typeof Card>
+} from "@/components/ui/card";
+import { Reveal } from "./Reveal";
+import { plans } from "@/mock-data/plans";
 
 export function PricingCards() {
   return (
     <div className="flex  gap-5 mt-4">
       {plans?.map((plan, index) => (
-        <Card key={index} className={`w-[300px] ${plan?.bgColor} ${plan?.borderColor}`}>
+        <Card
+          key={index}
+          className={`w-[300px] ${plan?.bgColor} ${plan?.borderColor}`}
+        >
           <CardHeader>
-            <CardTitle className="text-neutral-200 text-2xl">{plan?.type}</CardTitle>
-            {/* <CardDescription className="text-sm text-neutral-400">{plan?.description}</CardDescription> */}
+            <CardTitle className="text-neutral-200 text-2xl">
+              {plan?.type}
+            </CardTitle>
           </CardHeader>
           <CardContent className="grid gap-4">
             <div className="flex gap-1 text-white">
-              <div className="text-neutral-200 text-4xl flex flex-col justify-end">{plan?.price}</div>
-              <div className="text-sm flex items-center mt-3">{plan?.frequency}</div>
+              <div className="text-neutral-200 text-4xl flex flex-col justify-end">
+                {plan?.price}
+              </div>
+              <div className="text-sm flex items-center mt-3">
+                {plan?.frequency}
+              </div>
             </div>
             <div>
               {plan.features.map((feature, index) => (
@@ -79,5 +52,5 @@ export function PricingCards() {
         </Card>
       ))}
     </div>
-  )
+  );
 }
