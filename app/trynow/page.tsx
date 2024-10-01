c237dd03d688b20cb676e1f8417670e94fb33e7e
Pratim <hpro373@gmail.com>
2024-10-02 01:04:55 +0530

    Update page.tsx

--- app/trynow/page.tsx
+++ app/trynow/page.tsx
@@ -10,14 +10,14 @@ import Link from "next/link";
 import React, { useState } from "react";
 
 function Page() {
-  const [showVotes, setShowVotes] = useState(false);
+  const [showVotes, setShowVotes] = useState(true);
   return (
     <div className="relative min-h-screen flex flex-col  ">
       <div className=" flex justify-between w-full py-3 px-4 text-white ">
         <h2 className="bg-clip-text text-center bg-gradient-to-b   text-transparent  from-neutral-400 to-white text-3xl  font-bold tracking-tight">
           Estimate
         </h2>
-        <div className="flex gap-4 px-5  font-medium">
+        {/* <div className="flex gap-4 px-5  font-medium">
           {" "}
           <div className="flex items-end justify-end gap-4 z-[100]   text-sm  font-medium text-neutral-400    ">
             <div
@@ -32,7 +32,7 @@ function Page() {
               </div>
             </Link>
           </div>
-        </div>
+        </div> */}
       </div>
 
       <div className="   flex  px-4  gap-5 mt-[30px] relative">
