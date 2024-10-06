3315172e1dd132388d119e06d439b7f16b793dab
Pratim <hpro373@gmail.com>
2024-10-06 23:54:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
+import React, { Suspense } from "react";
 import Join from "@/components/join/Join";
 import Voting from "@/components/voting/Voting";
 import { useAppContext } from "@/context/AppContext";
 import { useMediaQuery } from "react-responsive";
+
 export default function Home() {
   const { createRoom, joinRoom } = useAppContext();
   const isSmallScreen = useMediaQuery({ maxWidth: 856 });
 
   return (
-    <>{createRoom.roomCode || joinRoom.roomCode ? <Voting /> : <Join />}</>
+    <Suspense fallback={<div>Loading...</div>}>
+      {createRoom.roomCode || joinRoom.roomCode ? <Voting /> : <Join />}
+    </Suspense>
   );
 }
